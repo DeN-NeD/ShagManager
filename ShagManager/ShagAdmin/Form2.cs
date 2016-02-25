f524e3831e7d19dfb191ad10a78afadd569b1f46
SVP142 <svp-142@mail.ru>
2016-02-25 20:41:18 +0600

    Changed

---
+++ ShagManager/ShagAdmin/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShagAdmin
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- ShagManager/ShagAdmin/Form4.Designer.cs
+++ ShagManager/ShagAdmin/Form4.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ShagAdmin
 {
-    partial class Form1
+    partial class Form4
     {
         /// <summary>
         /// Required designer variable.
@@ -28,94 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.AccessList = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.Password = new System.Windows.Forms.TextBox();
-            this.Login = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // AccessList
-            // 
-            this.AccessList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.AccessList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.AccessList.FormattingEnabled = true;
-            this.AccessList.Location = new System.Drawing.Point(131, 132);
-            this.AccessList.Name = "AccessList";
-            this.AccessList.Size = new System.Drawing.Size(121, 24);
-            this.AccessList.TabIndex = 6;
-            // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button1.Location = new System.Drawing.Point(284, 133);
+            this.button1.Location = new System.Drawing.Point(233, 148);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 18;
+            this.button1.TabIndex = 12;
             this.button1.Text = "Ок";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // Password
+            // textBox2
             // 
-            this.Password.Location = new System.Drawing.Point(131, 106);
-            this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(228, 20);
-            this.Password.TabIndex = 17;
+            this.textBox2.Location = new System.Drawing.Point(96, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(228, 20);
+            this.textBox2.TabIndex = 11;
             // 
-            // Login
+            // textBox1
             // 
-            this.Login.Location = new System.Drawing.Point(131, 68);
-            this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(228, 20);
-            this.Login.TabIndex = 16;
+            this.textBox1.Location = new System.Drawing.Point(96, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 20);
+            this.textBox1.TabIndex = 10;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label3.Location = new System.Drawing.Point(78, 68);
+            this.label3.Location = new System.Drawing.Point(43, 65);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(47, 17);
-            this.label3.TabIndex = 15;
+            this.label3.TabIndex = 9;
             this.label3.Text = "Login:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(52, 106);
+            this.label2.Location = new System.Drawing.Point(21, 103);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(73, 17);
-            this.label2.TabIndex = 14;
+            this.label2.TabIndex = 8;
             this.label2.Text = "Password:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.label1.Location = new System.Drawing.Point(117, 25);
+            this.label1.Location = new System.Drawing.Point(80, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(252, 22);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "Регистрация учетной записи";
+            this.label1.Size = new System.Drawing.Size(254, 22);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Авторизачия учетной записи";
             // 
-            // Form1
+            // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(501, 278);
+            this.ClientSize = new System.Drawing.Size(391, 235);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.Password);
-            this.Controls.Add(this.Login);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.AccessList);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Form4";
+            this.Text = "Form4";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,13 +111,11 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox AccessList;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox Password;
-        private System.Windows.Forms.TextBox Login;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
     }
 }
-
